5e681af232db61e5b3f5c43a6eb3ba856cd29b7d
Tmac-1 <1906526613@qq.com>
2021-02-02 18:43:07 +0800

    修改

--- test.js
+++ test.js
@@ -69,8 +69,8 @@
 // }
 
 
-var  arr = [3,4,2,5,1,8,4,9,9,2]
-
+// var  arr = [3,4,2,5,1,8,4,9,9,2]
+      //        4
 // function bubleSort(arr) {
 //   for (var i = 0; i < arr.length - 1; i++) {
 //     for (var j = i + 1; j < arr.length; j++) {
@@ -83,21 +83,155 @@ var  arr = [3,4,2,5,1,8,4,9,9,2]
 // }
 // console.log(bubleSort(arr)) 
     // i=2 j=1 j+1=2
-    function insertSort(arr){
-       for(var i=1;i<arr.length;i++){
-           var tem = arr[i]
-           var j = i-1
-           while(j>=0 && arr[j]>tem){
-              arr[j+1]=arr[j]
-              j--;
-           }
-           arr[j+1]=tem
-       }
-       return arr
-    }
-    console.log(insertSort(arr)) 
+   //  function insertSort(arr){
+   //     for (var i=1; i<arr.length; i++){
+   //        var j = i-1;
+   //        var tem = arr[i];
+   //        while(j>=0 && arr[j]>tem){
+   //              arr[j+1]=arr[j]
+   //              j--
+             
+   //        }
+   //        arr[j+1]=tem;
+   //     }
+   //     return arr
+   //  }
+   //  console.log(insertSort(arr))
+   // function quickSort(arr){
+   //    if(arr.length<=1){
+   //       return arr
+   //    }
+   //    let index = Math.floor(arr.length/2);
+   //    var tem = arr.splice(index,1) 
+   //    var left = [], right = []
+   //    for(var i=0;i<arr.length;i++){
+   //       if(arr[i]>tem){
+   //          right.push(arr[i])
+   //       }else{
+   //          left.push(arr[i])
+   //       }
+   //    }
+   //    return quickSort(left).concat(tem,quickSort(right))
+   // }
+   // console.log(quickSort(arr))
+// var i = 1;
+// console.log(++i,i)
+
+// var arr = [1,2,3,4,5]
+// console.log(arr.splice(1,1),arr)
+// console.log(arr.slice(1,2),arr) 
+
+// arr.sort((a,b)=>{
+//    return a-b
+// })
+// console.log(arr)
+
+// var arr01 = [1,2,3]
+// function shuffle(arr){
+//    for(var i=0;i<arr.length;i++){
+//       let j = Math.floor(Math.random() * arr.length);
+//       [arr[i],arr[j]]=[arr[j],arr[i]]
+//    }
+//    return arr
+// }
+// shuffle(arr01)
+
+// 
+//  var arr=[1,[2,3],[1,1],[4,[5,6,[7,8]]]]
+//  function flat(arr){
+//     for(var i=0; i<arr.length; i++){
+//         if(Array.isArray(arr[i])){
+//            arr = [].concat(...arr)
+//         }
+//     }
+//     return arr
+//  }
+
+//  console.log(flat(arr))
+
+// 斐波拉契
+// function fib(n){
+//    if(n==1 || n==2) return 1;
+//    return fib(n-1)+fib(n-2)
+// }
+// console.log(fib(10))
+
+// 二分查找
+// var arr = [1,2,3,3,3,4,4,4,5,6,7,8]
+
+// function binaryFind(num,arr,start,end){
+//    if(start<=end){
+//       if(num==arr[start]){
+//          return start
+//       }
+//       if(num==arr[end]){
+//          return end
+//       }
+//       let mid = Math.ceil((start+end)/2)
+//       if(num == arr[mid]){
+//          let findArr = []
+//          findArr.push(mid)
+//          let tem = mid;
+//          while(tem++){
+//             if(num==arr[tem]){
+//                findArr.push(tem)
+//             }else{
+//                break;
+//             }
+//          }
+//          tem = mid;
+//          while(tem--){
+//             if(num==arr[tem]){
+//                findArr.unshift(tem)
+//             }else{
+//                break;
+//             }
+//          }
+//          return findArr
+//       }
+//       if(num<arr[mid]){
+//         return binaryFind(num,arr,0,mid-1)
+//       }
+//       if(num>arr[mid]){
+//         return binaryFind(num,arr,mid+1,end)
+//       }
+//    }
+//    return -1
+// }
+// console.log(binaryFind(3,arr,0,arr.length-1)) 
 
 
+
+
+function legalBrackets(str){
+   let stack = []
+   let map = {
+      '{':'}',
+      '[':']',
+      '(':')',
+   }
+   for(let item of str) {
+      if(map[item]) {
+         stack.push(item)
+      }else if(Object.values(map).includes(item)){
+         let last = stack.pop()
+         if(item !== map[last]) return false
+      }
+   }
+   return stack.length == 0
+}
+console.log(legalBrackets("()")); //true
+console.log(legalBrackets("([ ) ]")); //false
+console.log(legalBrackets("([{ )]")); //false
+console.log(legalBrackets("()[ ]{}")); //true
+console.log(legalBrackets(" { ]")); //false
+console.log(legalBrackets("{ [ ] }")); //true
+console.log(legalBrackets("function a (){console.log(1111)}"))// true
+
+
+
+// let arr = [1,2,3]
+// console.log(arr.pop(),arr)
 
 
 
