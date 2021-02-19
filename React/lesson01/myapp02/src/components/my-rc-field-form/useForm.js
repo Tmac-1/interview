d2b17e61084ffef34b72857bd18e420613825545
Tmac-1 <1906526613@qq.com>
2021-02-20 00:28:42 +0800

    1111

--- React/lesson01/myapp02/src/components/my-rc-field-form/useForm.js
+++ React/lesson01/myapp02/src/components/my-rc-field-form/useForm.js
@@ -1,5 +1,102 @@
-import React from 'react';
 
-export default function useForm(props) {
-  return []
+import React,{useRef} from 'react';
+
+class FormStore {
+  constructor() {
+    this.store = {} // 存儲數據，比如説username password
+    this.fieldEnetities = []
+    this.callbacks = {}
+  }
+  registryEntity = entity => {
+    this.fieldEnetities.push(entity);
+    return ()=> {  // 返回函数，用于取消订阅
+      this.fieldEnetities = this.fieldEnetities.filter(item => item !== entity);
+      delete this.store[entity.props.name]
+    }
+  }
+  setFieldsValue = (newStore)=>{
+    this.store = {
+      ...this.store,
+      ...newStore
+    }
+    console.log('store',this.store)
+    // 对应组件需要更新
+    this.fieldEnetities.forEach(entity => {
+      const {name}=entity.props;
+      Object.keys(newStore).forEach(key => {
+        if(key === name){
+          entity.onStoreChange()
+        }
+      }) 
+    })
+  };
+  getFieldValue = name => {
+    return this.store[name]
+  }
+  getFieldsValue = () => {
+    return this.store
+  }
+  setCallback = callback => {
+    this.callbacks = {
+      ...this.callbacks,
+      ...callback
+    }
+  }
+  validate = () => {
+    let err = [];
+    // todo
+    this.fieldEnetities.forEach(entity => {
+      const { name, rules } = entity.props;
+      let value = this.getFieldValue(name);
+      let rule = rules && rules[0];
+      console.log(name, rules,value,rule,this.store)
+      if (rule && rule.required && (value === undefined || value === "")) {
+        // 出错
+        err.push({
+          [name]: rules.message,
+          value
+        });
+      }
+    });
+    return err;
+  };
+
+  submit = () => {
+    console.log("this.", this.fieldEnetities); //sy-log
+    let err = this.validate();
+    // 在这⾥校验 成功的话 执⾏onFinish ，失败执⾏onFinishFailed
+    const { onFinish, onFinishFailed } = this.callbacks;
+    if (err.length === 0) {
+      // 成功的话 执⾏onFinish
+      onFinish(this.getFieldsValue());
+    } else if (err.length > 0) {
+      // ，失败执⾏onFinishFailed
+      onFinishFailed(err);
+    }
+  };
+
+  getForm(){
+    return {
+      setFieldsValue: this.setFieldsValue,
+      getFieldValue: this.getFieldValue,
+      registryEntity: this.registryEntity,
+      submit: this.submit,
+      setCallback: this.setCallback
+    }
+  }
+}
+
+
+
+export default function useForm(form) {
+  const formRef = useRef();
+  if(!formRef.current){
+    if(form) {
+      formRef.current = form;
+    }else{
+      const formStore = new FormStore();
+      formRef.current = formStore.getForm()
+    }
+  }
+  return [formRef.current]
 }
--- React/lesson01/myapp02/src/pages/MyRCFieldForm.js
+++ React/lesson01/myapp02/src/pages/MyRCFieldForm.js
@@ -1,46 +1,75 @@
 import React, { Component, useEffect } from "react";
 // import Form, { Field } from "rc-field-form";
-import Form, {Field} from "../components/my-rc-field-form/";
+import Form, { Field } from "../components/my-rc-field-form/";
 // import Input from '../components/Input';
 import { Input, Button } from "antd";
 
 const nameRules = { required: true, message: "请输⼊姓名！" };
 const passworRules = { required: true, message: "请输⼊密码！" };
 
-export default class MyRCFieldForm extends Component {
-    formRef = React.createRef();
-    componentDidMount() {
-        console.log("form", this.formRef.current); //sy-log
-        // this.formRef.current.setFieldsValue({ username: "default" });
-    }
-    onFinish = val => {
+export default function MyRCFieldForm(props) {
+    const [form] = Form.useForm();
+    const onFinish = val => {
         console.log("onFinish", val); //sy-log
     };
     // 表单校验失败执⾏
-    onFinishFailed = val => {
+    const onFinishFailed = val => {
         console.log("onFinishFailed", val); //sy-log
     };
-    render() {
-        return (
-            <div>
-                <h3>MyRCFieldForm</h3>
-                <Form
-                    ref={this.formRef}
-                    onFinish={this.onFinish}
-                    onFinishFailed={this.onFinishFailed}>
-                    开课吧web全栈架构师
-                    实现my-rc-field-form
-                    实现Form/index
-                    实现Form
-        <Field name="username" rules={[nameRules]}>
-                        <Input placeholder="Username" />
-                    </Field>
-                    <Field name="password" rules={[passworRules]}>
-                        <Input placeholder="Password" />
-                    </Field>
-                    <button>Submit</button>
-                </Form>
-            </div>
-        );
-    }
+    useEffect(() => {
+        console.log("form", form); //sy-log
+        form.setFieldsValue({ username: "default" });
+    }, []);
+    return (
+        <div>
+            <h3>MyRCFieldForm</h3>
+            <Form form={form} onFinish={onFinish} onFinishFailed={onFinishFailed}>
+                <Field name="username" rules={[nameRules]}>
+                    <Input placeholder="input UR Username" />
+                </Field>
+                <Field name="password" rules={[passworRules]}>
+                    <Input placeholder="input UR Password" />
+                </Field>
+                <button>Submit</button>
+            </Form>
+        </div>
+    );
 }
+
+// export default class MyRCFieldForm extends Component {
+//     formRef = React.createRef();
+//     componentDidMount() {
+//         console.log("form", this.formRef.current); //sy-log
+//         // this.formRef.current.setFieldsValue({ username: "default" });
+//     }
+//     onFinish = val => {
+//         console.log("onFinish", val); //sy-log
+//     };
+//     // 表单校验失败执⾏
+//     onFinishFailed = val => {
+//         console.log("onFinishFailed", val); //sy-log
+//     };
+//     render() {
+//         return (
+//             <div>
+//                 <h3>MyRCFieldForm</h3>
+//                 <Form
+//                     ref={this.formRef}
+//                     onFinish={this.onFinish}
+//                     onFinishFailed={this.onFinishFailed}>
+//                     开课吧web全栈架构师
+//                     实现my-rc-field-form
+//                     实现Form/index
+//                     实现Form
+//         <Field name="username" rules={[nameRules]}>
+//                         <Input placeholder="Username" />
+//                     </Field>
+//                     <Field name="password" rules={[passworRules]}>
+//                         <Input placeholder="Password" />
+//                     </Field>
+//                     <button>Submit</button>
+//                 </Form>
+//             </div>
+//         );
+//     }
+// }
