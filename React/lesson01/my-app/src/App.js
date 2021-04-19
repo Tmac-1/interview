0f75b628d81136ec02c6d8179cb309e45d54bc9d
Tmac-1 <1906526613@qq.com>
2021-04-19 17:24:34 +0800

    提交

--- React/lesson01/my-app/src/App.js
+++ React/lesson01/my-app/src/App.js
@@ -2,10 +2,39 @@ import React, { Component } from "react";
 import ContextPage from "./pages/ContextPage";
 import { Demo, DemoState, DemoState2, DemoUseMemo } from "./pages/hooks";
 
+
+class Test extends Component {
+  state = {
+    num: 1
+  }
+  handleClick = ()=>{
+    this.setState({
+      num: this.state.num++
+    })
+    this.setState({
+      num: this.state.num++
+    })
+    // this.setState({
+    //   num: ++this.state.num
+    // })
+    // this.setState({
+    //   num: ++this.state.num
+    // })
+    console.log(this.state.num)
+  }
+  render(){
+    // console.log('render')
+    let {num} = this.state
+    return <div>
+      <button onClick={this.handleClick}>{num}test1</button>
+    </div>
+  }
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Test/>
         <Demo />
         <DemoState />
         <DemoState2 />
