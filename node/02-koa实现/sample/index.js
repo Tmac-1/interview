f01d1fd39ce0ffe8da60938d41f5102c79c656fe
renbinbin <renbinbin@didiglobal.com>
2021-07-06 21:14:11 +0800

    修改

--- node/02-koa实现/sample/index.js
+++ node/02-koa实现/sample/index.js
@@ -1,5 +1,8 @@
 const Koa = require('koa')
 const app = new Koa()
+const Router = require('koa-router');
+
+var router = new Router();
 
 app.use(async (ctx, next) => {
     const start = Date.now()
@@ -8,13 +11,28 @@ app.use(async (ctx, next) => {
     console.log(`请求${ctx.url} 耗时${parseInt(end - start)}ms`)
 })
 
-app.use((ctx, next) => {
-    const expire = Date.now() + 102
-    while(Date.now() < expire) {}
+// app.use((ctx, next) => {
+//     const expire = Date.now() + 102
+//     while(Date.now() < expire) {}
+//     ctx.body = {
+//         name: 'tom'
+//     }
+// })
+
+router.get('/test',(ctx, next) => {
+    //   ctx.body = 'hello test'
     ctx.body = {
-        name: 'tom'
+        errorCode: 1,
+        data: {
+            name: 111
+        }
     }
 })
+
+//使用路由中间件
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(3000, () => {
     console.log('koa start')
